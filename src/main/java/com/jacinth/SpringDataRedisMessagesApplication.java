158b4ea6403bdba92b67bfba49d73eb31f270658
JacinthEsther <agbonirojacinta@gmail.com>
2024-02-19 15:32:10 +0100

    redis-messaging

---
+++ src/main/java/com/jacinth/SpringDataRedisMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.jacinth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisMessagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisMessagesApplication.class, args);
+    }
+
+}
